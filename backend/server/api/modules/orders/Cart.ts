b9ebb816917e53f168cc19f2d85983a5dcfb72c0
dmarinsa <dmarinsanchez@gmail.com>
2021-10-24 01:19:07 +0200

    delete unused var

--- backend/server/api/modules/orders/Cart.ts
+++ backend/server/api/modules/orders/Cart.ts
@@ -1,6 +1,5 @@
 import {
   Item,
-  OrderDto,
   Currency,
   CurrencySymbols,
   OrderedProduct,
@@ -11,7 +10,13 @@ export class Cart {
   readonly items: Item[] = [];
   grandTotal: number;
   readonly discountedGrandTotal?: number;
-  constructor(readonly currency: Currency) {}
+  private currencySymbol: string;
+  constructor(readonly currency: Currency) {
+    this.currencySymbol =
+      this.currency === Currency.EUR
+        ? CurrencySymbols.EUR
+        : CurrencySymbols.USD;
+  }
 
   private sumGrandTotal(): number {
     return this.items.reduce((acc, curr) => {
@@ -22,15 +27,64 @@ export class Cart {
   }
 
   addLine(item: OrderedProduct): void {
-    const currencySymbol =
-      this.currency === Currency.EUR
-        ? CurrencySymbols.EUR
-        : CurrencySymbols.USD;
     this.items.push({
       id: item.id,
       name: item.name,
-      individualPrice: `${currencySymbol}${item.cost}`,
-      totalPrice: `${currencySymbol}${item.cost * item.quantity}`,
+      individualPrice: `${this.currencySymbol}${item.cost}`,
+      totalPrice: `${this.currencySymbol}${item.cost * item.quantity}`,
+    });
+  }
+
+  applyOffers(): void {
+    const day = new Date().getDay();
+    this.applySoupAndBreadBOGOF();
+    this.applySundaySoupSale(day);
+    this.applyDairyDelicious();
+  }
+
+  private applySoupAndBreadBOGOF(): void {
+    let soupsCount = 0;
+    let breadsCount = 0;
+
+    this.items.forEach((item) => {
+      if (!['Soup', 'Bread'].includes(item.name)) return;
+      item.name === 'Soup' ? ++soupsCount : ++breadsCount;
+    });
+
+    const freeSoupLimit = Math.min(soupsCount, breadsCount, 3);
+
+    for (let i = 0; i < freeSoupLimit; i++) {
+      this.items.push({
+        id: 1,
+        name: 'Free Soup',
+        individualPrice: `${this.currencySymbol}0`,
+        totalPrice: `${this.currencySymbol}0`,
+      });
+    }
+  }
+
+  private applySundaySoupSale(day: number): void {
+    if (day !== 0) return;
+
+    let soupsCount = 0;
+    let breadsCount = 0;
+
+    this.items.forEach((item) => {
+      if (!['Soup', 'Bread'].includes(item.name)) return;
+      item.name === 'Soup' ? ++soupsCount : ++breadsCount;
+    });
+  }
+
+  private applyDairyDelicious(): void {
+    for (const item of this.items) {
+      if (item.name === 'Free Soup') return;
+    }
+    let soupsCount = 0;
+    let breadsCount = 0;
+
+    this.items.forEach((item) => {
+      if (!['Soup', 'Bread'].includes(item.name)) return;
+      item.name === 'Soup' ? ++soupsCount : ++breadsCount;
     });
   }
 
